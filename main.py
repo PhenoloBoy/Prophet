97df4dd2d8fad9f14dfd3a6bc2f5df046857908c
pl.marasco@gmail.com <pl.marasco@gmail.com>
2019-02-18 17:26:00 +0100

    varii cambiamenti

--- main.py
+++ main.py
@@ -1,21 +1,56 @@
-# import fbprophet
+from fbprophet import Prophet
 import xarray as xr
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
 
 
 def main():
-    data = xr.open_rasterio(r'c:\temp\chianti.img')
+    data = xr.open_rasterio(r'C:\Users\Pier\PycharmProjects\Prophet\data\chianti.img')
     ndvi = data[:, 0, 0]
 
     dates = pd.date_range('1/1/1998', '31/12/2013')
     dek = dates[dates.day.isin([1, 11, 21])]
     dek = dek[:ndvi.shape[0]]
 
-    d = {'time': dek, 'ndvi': ndvi}
-    df = pd.DataFrame(data=d)
+    ts = pd.Series(ndvi, index=dek)
 
-    print(df)
+    ts[ts.isin(range(250, 256))] = pd.NaT
+    # tsd = ts.resample('D').interpolate(method='linear').to_frame(name='y')
+
+    tsd = ts.to_frame(name='y')
+
+    tsd.reset_index(inplace=True)
+    tsd.rename(columns={'index': 'ds'}, inplace=True)
+
+    m = Prophet(growth='linear', yearly_seasonality=True)
+    m.fit(tsd)
+
+    change = tsd[tsd['ds'].isin(m.changepoints)]
+    register_matplotlib_converters()
+
+    future = m.make_future_dataframe(periods=600)
+    future.tail()
+    forecast = m.predict(future)
+
+    fig1 = m.plot(forecast)
+    fig2 = m.plot_components(forecast)
+    #
+
+    tsd.set_index(pd.to_datetime(tsd['ds'], unit='s'), inplace=True)
+
+    points = tsd[tsd['ds'].isin(m.changepoints)]
+
+    plt.figure(3)
+    tsd['y'].plot()
+    points['y'].plot()
+
+    # plt.show(fig1)
+    # plt.plot(fig2)
+
+    plt.show()
+
+
 
 
 if __name__ == '__main__':
